a7f721fcc4e94238e94a1636163c8fc596132857
Rob Whiting <rob.t.whiting@gmail.com>
2018-06-23 17:38:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  var newObj = Objet.assign({}, obj)
+  delete newObj.foo
+  return newObj
 }
